7284561def54b06135604344b5d9898f9a677808
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2025-09-06 14:40:44 +0530

    updated admin frontend

--- admin-frontend/src/app/app.routes.ts
+++ admin-frontend/src/app/app.routes.ts
@@ -6,14 +6,18 @@ import { Projects } from './pages/projects/projects';
 import { Products } from './pages/products/products';
 import { AddProducts } from './components/add-products/add-products';
 import { UpdateProduct } from './components/update-product/update-product';
+import { Tags } from './pages/tags/tags';
+import { Login } from './components/login/login';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'login', component: Login },
   {
     path: 'dashboard',
     component: Dashboard,
     children: [
       { path: 'categories', component: Categories },
+      { path: 'tags', component: Tags },
       { path: 'materials', component: Materials },
       { path: 'projects', component: Projects },
       { path: 'products', component: Products, children: [
